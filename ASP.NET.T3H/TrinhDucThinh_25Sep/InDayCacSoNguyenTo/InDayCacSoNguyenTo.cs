25c5bcfa36dad65c570c86a4ae27c9f43327d7fd
THINH_PC\thinhtd <tdthinh1995@gmail.com>
2016-09-27 16:41:19 +0700

    Trinh Duc Thinh add pull

---
+++ ASP.NET.T3H/TrinhDucThinh_25Sep/InDayCacSoNguyenTo/InDayCacSoNguyenTo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InDayCacSoNguyenTo
+{
+    class InDayCacSoNguyenTo
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                //Nhap so bat dau va so ket thuc
+
+                Console.WriteLine("Nhap so bat dau");
+                int start = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Nhap so bat dau");
+                int end = Convert.ToInt32(Console.ReadLine());
+
+                //kiem tra va dua ra ket qua
+                Console.WriteLine("Danh sach cac so nguyen to tu {0} den {1}", start, end);
+                for (int i = start; i <= end; i++)
+                {
+                    if (KiemTraSoNguyenTo.KiemTraSoNguyenTo.IsPrime(i)) Console.WriteLine("{0}", i);
+                }
+
+                //Hoi y kien nguoi dung
+                Console.WriteLine("Nhan enter de tiep tuc. Nhan Q de thoat");
+                string ch = Console.ReadLine();
+                if (ch.ToUpper() == "Q") break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ASP.NET.T3H/TrinhDucThinh_25Sep/StringReverse/DaoNguocChuoi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverse
+{
+    class DaoNguocChuoi
+    {
+        
+        #region Main
+
+        static void Main(string[] args)
+        {
+            //Nhap vao mot chuoi
+            Console.WriteLine("Nhap vao mot chuoi");
+            string str = Console.ReadLine();
+
+            //xu ly dua ket qua vao strReverse
+            string strReverse = ReverseString(str);
+
+            //Hien thi
+            Console.WriteLine("{0} chuoi dao nguoc {1}", str, strReverse);
+            Console.ReadLine();
+
+        }
+        #endregion
+      
+
+        #region Method static
+        //Phuong thuc dao nguoc chuoi
+        private static string ReverseString(string str)
+        {
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr); // đảo ngược mãng
+            return new string(arr); // trả về chuỗi mới sau khi đảo mãng
+
+        }
+        #endregion
+       
+    }
+}
